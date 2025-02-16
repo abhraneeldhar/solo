905c893a89f71aea4c319fed2f13e7a900a71a9f
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-16 19:58:50 +0530

    cards added n yah

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,69 @@
 import { Input } from "@/components/ui/input"
 import styles from "./home.module.css"
 import { Terminal } from "lucide-react"
-import python from "../public/languageLogos/python.png"
-import Image from "next/image"
-export default function Home() {
-  return (<>
-  <div className={styles.homemain}>
-    <h1 className={styles.bigHeader}>solo <span>Dev</span></h1>
-    <p className={styles.headerPara}>Quick reference guides for popular programming languages and frameworks.<br/> Master your tools with comprehensive cheatsheets</p>
-    <input className={styles.searchInput} placeholder="Search for popular languages and frameworks..."/>
+import Image, { StaticImageData } from "next/image"
 
-    <div className={styles.languageCardHolder}>
-      <div className={styles.languageCard}>
-        <Terminal className={styles.terminalIcon}/>
-        <div className={styles.logo}>
-          <Image src={python} alt="py" />
+import python from "../public/languageLogos/python.png"
+import javascript from "../public/languageLogos/javascript.png"
+import css from "../public/languageLogos/css.png"
+import html from "../public/languageLogos/html.png"
+import nextjs from "../public/languageLogos/nextjs.png"
+interface cardDetailType {
+  name: string,
+  image: StaticImageData,
+  alt: string
+}
+
+export default function Home() {
+  const cards: cardDetailType[] = [{
+    name: "Python",
+    image: python,
+    alt: "py"
+  }, {
+    name: "Javascript",
+    image: javascript,
+    alt: "js"
+  },
+  {
+    name:"CSS",
+    image:css,
+    alt:"css"
+  },
+  {
+    name:"HTML",
+    image:html,
+    alt:"html"
+  }, {
+    name:"NEXTjs",
+    image:nextjs,
+    alt:"next"
+  } 
+]
+
+  const DisplayCard = ({ cardInfo }: { cardInfo: cardDetailType }) => {
+    return (<>
+      <div className={`${styles.languageCard} ${styles[cardInfo.name]}`}>
+        <div className={styles.terminalIconDiv} >
+          <Terminal className={styles.terminalIcon} />
         </div>
-        <h1>Python</h1>
+        <div className={styles.logo}>
+          <Image unoptimized src={cardInfo.image} alt={cardInfo.alt} />
+        </div>
+        <h1>{cardInfo.name}</h1>
+      </div>
+    </>)
+  }
+  return (<>
+    <div className={styles.homemain}>
+      <h1 className={styles.bigHeader}>solo <span>Dev</span></h1>
+      <p className={styles.headerPara}>Quick reference guides for popular programming languages and frameworks.<br /> For devs with noob batchmates</p>
+      <input className={styles.searchInput} placeholder="Search for popular languages and frameworks..." />
+
+      <div className={styles.languageCardHolder}>
+
+        {cards.map((cardInfo) => (<DisplayCard cardInfo={cardInfo} />))}
+
       </div>
     </div>
-  </div>
   </>)
 }
